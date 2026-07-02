b53373b49eaf9494984b21dbb3f37ff304b2ab24
sebastian-helguero <sebastianhelguero03@gmail.com>
2026-07-02 15:40:58 -0300

    commit 6.1

--- src/GymManagement/Program.cs
+++ src/GymManagement/Program.cs
@@ -103,6 +103,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x=>x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); 
+
 app.UseAuthentication();
 app.UseAuthorization();
 
